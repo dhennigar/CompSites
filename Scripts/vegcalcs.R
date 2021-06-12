a1e9620f589a9cf2282caa17db18508f371c374e
dhennigar <dhennigar@protonmail.com>
2021-06-11 18:39:03 -0700

    First commit of vegcalcs code
    Edited vegcalcs_example.R to fit 2021 comp site data.

--- Scripts/vegcalcs.R
+++ Scripts/vegcalcs.R
@@ -1,52 +1,66 @@
-#R code for shifting data from long to wide, calculating richness/plot, and simpson's and shannon's diversity/plot. For this example I have two datasets (Frenchies and Reference) for example. 
+#R code for shifting data from long to wide, calculating richness/plot, and simpson's and shannon's diversity/plot. For this example I have two datasets (Frenchies and Ref) for example. 
 #Loading Libraries
 library("dplyr")
 library("tidyverse")
 library("BiodiversityR")
 
 
-#CREATION OF FRENCHIES DATASET
-#note that the test.cv was now absorbed into the 2nd sheet of REFERENCEMASTER.xls
-Frenchies <- read.csv("~/Git/TyphaData/FieldData/JULY2019_DIVERSITY.csv")
-Frenchies$PERCENT_COVER = as.numeric(Frenchies$PERCENT_COVER)
-Frenchies$PERCENT_COVER = Frenchies$PERCENT_COVER*10
-
-Reference<- read.csv("~/Git/TyphaData/FieldData/REFERENCE_DIVERSITY.csv") %>%
-  na.omit(JULY2020)
-Reference$PERCENT_COVER = as.numeric(Reference$PERCENT_COVER)
-Reference$PERCENT_COVER = Reference$PERCENT_COVER*10
+#create and prep Comp dataset and subsets
+Comp <- read.csv("C:/Users/Owner/OneDrive/Documents/GitHub/CompSites/FieldData/09-006.csv")
+Comp$PERCENT_COVER = as.numeric(Comp$PERCENT_COVER)
+Comp <- subset(Comp, select = c(-COMMENTS, -ï..Site_Number, -MAX_LH_CM), Comp$COMMUNITY == 1) # Community 1, all species subset
+Comp.N <- subset(Comp, Comp$ORIGIN == "N", select = c(-ORIGIN, -COMMUNITY)) # native species subset
+Comp <- subset(Comp, select = c(-ORIGIN, -COMMUNITY)) # remove origin column
 
 
-#transforming dataset into wide format for richness calcs
-FrenchiesWIDE <- Frenchies %>%
-  spread(SPECIES, PERCENT_COVER)
-FrenchiesWIDE[is.na(FrenchiesWIDE)] <- 0
-FrenchiesWIDE = FrenchiesWIDE[-1]
+#create and prep reference site dataset
+Ref <- read.csv("C:/Users/Ownder/OneDrive/Documents/GitHub/FieldData/REFERENCE_DIVERSITY.csv")
+Ref$PERCENT_COVER = as.numeric(Ref$PERCENT_COVER)
+Ref <- subset(Ref, select = c(-COMMENTS, -ï..Site_Number, -MAX_LH_CM), Ref$COMMUNITY == 1)
+Ref.N <- subset(Ref, Ref$ORIGIN == "N", select = c(-ORIGIN, -COMMUNITY))
+Ref <- subset(Ref, select = c(-ORIGIN, -COMMUNITY)) 
 
-ReferenceWIDE <- Reference %>%
-  spread(SPECIES, PERCENT_COVER)
-ReferenceWIDE[is.na(ReferenceWIDE)] <- 0
-ReferenceWIDE = ReferenceWIDE[-1]
+#transforming datasets into wide format for richness calcs
+Comp.Wide <- Comp %>%
+  spread(SPECIES_CODE, PERCENT_COVER)
+Comp.Wide[is.na(Comp.Wide)] <- 0
+Comp.Wide = Comp.Wide[-1]
+
+Comp.N.Wide <- Comp.N %>%
+  spread(SPECIES_CODE, PERCENT_COVER)
+Comp.N.Wide[is.na(Comp.N.Wide)] <- 0
+Comp.N.Wide = Comp.N.Wide[-1]
+
+Ref.Wide <- Ref %>%
+  spread(SPECIES_CODE, PERCENT_COVER)
+Ref.Wide[is.na(Ref.Wide)] <- 0
+Ref.Wide = Ref.Wide[-1]
+
+Ref.N.Wide <- Ref.N %>%
+  spread(SPECIES_CODE, PERCENT_COVER)
+Ref.N.Wide[is.na(Ref.N.Wide)] <- 0
+Ref.N.Wide = Ref.N.Wide[-1]
 
 #RICHNESS CALCS
-Frenchiesrichness <- specnumber(FrenchiesWIDE)
-Referencerichness <- specnumber(ReferenceWIDE)
+CompRichness <- specnumber(Comp.Wide)
+CompRichness.N <- specnumber(Comp.N.Wide)
+RefRichness <- specnumber(Ref.Wide)
+RefRichness.N <- specnumber(Ref.N.Wide)
 
 #DIVERSITY CALCS
 #SIMPSONS
-Frenchiessimp <- diversity(FrenchiesWIDE, "simpson")
-Referencesimp <- diversity(ReferenceWIDE, "simpson")
+CompSimp <- diversity(Comp.Wide, "simpson")
+RefSimp <- diversity(Ref.Wide, "simpson")
 #SHANNONS
-Frenchiesshann <- diversity(ReferenceWIDE, "shannon")
-Referenceshann <- diversity(ReferenceWIDE, "shannon")
+CompShan <- diversity(Comp.Wide, "shannon")
+RefShan <- diversity(Ref.Wide, "shannon")
 
 #calculating mean simpsons diversity of each site
-mean(Frenchiessimp)
-mean(Referencesimp)
-mean(F)
+mean(CompSimp)
+mean(RefSimp)
 
-#export diversity tables (Example)
-write.csv(Frenchiessimp, file ="FILEPATH" )
+#export diversity tables
+write.csv(CompSimp, file ="FILEPATH" )
 
 
 
