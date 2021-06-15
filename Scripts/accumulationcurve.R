eb413c28ef961e4ac9709a62e44ecea40d9bcbe4
dhennigar <dhennigar@protonmail.com>
2021-06-15 16:54:29 -0700

    Plots three curves
    The plot now includes the two reference curves as well

--- Scripts/accumulationcurve.R
+++ Scripts/accumulationcurve.R
@@ -7,6 +7,7 @@ library("ggplot2")
 
 
 ### IMPORT DATA ###
+# edit file paths as necessary
 
 # Compensation Site
 Comp <- read.csv("C://Users/Owner/OneDrive/Documents/GitHub/CompSites/FieldData/09-006.csv", fileEncoding = "UTF-8-BOM")
@@ -16,12 +17,12 @@ Comp$PERCENT_COVER = as.numeric(Comp$PERCENT_COVER)
 Comp <- subset(Comp, ORIGIN == "N", select = c(-ORIGIN, -Site_Number, -COMMENTS, -MAX_LH_CM, -COMMUNITY)) # select native species only and remove extra columns
 
 # Reference Site 1
-Ref1 <- read.csv("FILEPATH", fileEncoding = "UTF-8-BOM")
+Ref1 <- read.csv("C://Users/Owner/OneDrive/Documents/GitHub/CompSites/FieldData/FAKE_REF1.csv", fileEncoding = "UTF-8-BOM")
 Ref1$PERCENT_COVER - as.numeric(Ref1$PERCENT_COVER)
 Ref1 <- subset(Ref1, ORIGIN == "N", select = c(-ORIGIN, -Site_Number, -COMMENTS, -MAX_LH_CM, -COMMUNITY))
 
 # Reference Site 2
-Ref2 <- read.csv("FILEPATH", fileEncoding = "UTF-8-BOM")
+Ref2 <- read.csv("C://Users/Owner/OneDrive/Documents/GitHub/CompSites/FieldData/FAKE_REF2.csv", fileEncoding = "UTF-8-BOM")
 Ref2$PERCENT_COVER - as.numeric(Ref2$PERCENT_COVER)
 Ref2 <- subset(Ref2, ORIGIN == "N", select = c(-ORIGIN, -Site_Number, -COMMENTS, -MAX_LH_CM, -COMMUNITY))
 
@@ -35,27 +36,27 @@ CompWIDE = CompWIDE[-1]
 
 Ref1WIDE <- Ref1 %>%
   spread(SPECIES_CODE, PERCENT_COVER)
-Ref1WIDE[is.na(RefWIDE)] <- 0
+Ref1WIDE[is.na(Ref1WIDE)] <- 0
 Ref1WIDE = Ref1WIDE[-1]
 
 Ref2WIDE <- Ref2 %>%
   spread(SPECIES_CODE, PERCENT_COVER)
-Ref2WIDE[is.na(RefWIDE)] <- 0
+Ref2WIDE[is.na(Ref2WIDE)] <- 0
 Ref2WIDE = Ref2WIDE[-1]
 
 ### CREATE SPECIES ACCUM CURVES
-CompAC <- specaccum(CompWIDE)
-Ref1AC <- specaccum(Ref1WIDE)
-Ref2AC <- specaccum(Ref2WIDE)
+CompRichness <- specaccum(CompWIDE)
+Ref1Richness <- specaccum(Ref1WIDE)
+Ref2Richness <- specaccum(Ref2WIDE)
 
 
-### SINGLE CURVE PLOT
-accumCurve <- plot(CompRichness, ci.type="poly", col="tomato4", lwd=2, ci.lty=0, ci.col=rgb(1,0,0,0.4), 
-              main = "", xlab="# of plots sampled", ylab= "# of native species", ylim =c(0,40))
+### PLOT SAC's
+plot(CompRichness, ci.type = "poly", col="tomato4", lwd = 2, ci.lty = 0, ci.col = rgb(1,0,0,0.4), 
+        main = "", xlab = "# of plots sampled", ylab = "# of native species", ylim = c(0,40))
 
-### PLOT FOR THREE CURVES
+lines(Ref1Richness, ci.type = "poly", ci.col = "cyan", lwd = 2, ci.lty = 0)
+lines(Ref2Richness, ci.type = "poly", ci.col = "green", lwd = 2, ci.lty = 0)
 
-ggplot()
 
 
 
