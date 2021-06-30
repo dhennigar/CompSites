49daf26686c69ac4c0408a3b3195f65487e3e30b
dhennigar <dhennigar@protonmail.com>
2021-06-29 17:15:13 -0700

    second revision of vegcalcs.R
    second revision better addresses the needs of our updated final spreadsheet.
    
    work in progress:
    - generate relative abundances
    - write results to .csv for easy compilation

--- Scripts/vegcalcs.R
+++ Scripts/vegcalcs.R
@@ -1,67 +1,55 @@
-#R code for shifting data from long to wide, calculating richness/plot, and simpson's and shannon's diversity/plot.
+# Revision 2 of veg community analysis
+# 29/June/2021
 
-library("dplyr")
-library("tidyverse")
-library("BiodiversityR")
+# libraries
+library(dplyr)
+library(tidyverse)
+library(BiodiversityR)
 
+# DATA IMPORT & PREP
 
-#create and prep Comp dataset and subsets
-Comp <- read.csv("~/Documents/GitHub/CompSites/FieldData/09-006.csv")
-Comp$PERCENT_COVER = as.numeric(Comp$PERCENT_COVER)
-Comp <- subset(Comp, select = c(-COMMENTS, -Site_Number, -MAX_LH_CM), Comp$COMMUNITY == 1) # Community 1, all species subset
-Comp.N <- subset(Comp, Comp$ORIGIN == "N", select = c(-ORIGIN, -COMMUNITY)) # native species subset
-Comp <- subset(Comp, select = c(-ORIGIN, -COMMUNITY)) # remove origin column
+# import (modify filepath)
+veg <- read.csv("C://Users/Owner/OneDrive/Documents/GitHub/CompSites/FieldData/09-006.csv",
+                fileEncoding = "UTF-8-BOM")
+veg$PERCENT_COVER <- as.numeric(veg$PERCENT_COVER) # ensure numeric cover data
+veg <- subset(veg, COMMUNITY == 1) # select only community 1
 
+# transform data for richness calculations
+veg.wide <- subset(veg, select = c(-COMMENTS, -COMMUNITY, -Site_Number, -MAX_LH_CM, -ORIGIN)) %>%
+  spread(SPECIES_CODE, PERCENT_COVER) # long to wide format
+veg.wide[is.na(veg.wide)] <- 0 # empty cells are zero
+veg.wide <- veg.wide[-1] # remove plot id column
 
-#create and prep reference site dataset
-Ref <- read.csv("~/Documents/GitHub/FieldData/REFERENCE_DIVERSITY.csv")
-Ref$PERCENT_COVER = as.numeric(Ref$PERCENT_COVER)
-Ref <- subset(Ref, select = c(-COMMENTS, -Site_Number, -MAX_LH_CM), Ref$COMMUNITY == 1)
-Ref.N <- subset(Ref, Ref$ORIGIN == "N", select = c(-ORIGIN, -COMMUNITY))
-Ref <- subset(Ref, select = c(-ORIGIN, -COMMUNITY)) 
+veg.nat.wide <- subset(veg, ORIGIN == "N", select = c(-COMMENTS, -COMMUNITY, # select natives only
+                                                      -Site_Number, -MAX_LH_CM, -ORIGIN))  %>%
+  spread(SPECIES_CODE, PERCENT_COVER) # long to wide format
+veg.nat.wide[is.na(veg.nat.wide)] <- 0 # empty cells are zero
+veg.nat.wide <- veg.nat.wide[-1] # remove plot id column
 
-#transforming datasets into wide format for richness calcs
-Comp.Wide <- Comp %>%
-  spread(SPECIES_CODE, PERCENT_COVER)
-Comp.Wide[is.na(Comp.Wide)] <- 0
-Comp.Wide = Comp.Wide[-1]
+veg.inv.wide <- subset(veg, ORIGIN == "I", select = c(-COMMENTS, -COMMUNITY, # select invasives only
+                                                      -Site_Number, -MAX_LH_CM, -ORIGIN)) %>%
+  spread(SPECIES_CODE, PERCENT_COVER) # long to wide format
+veg.inv.wide[is.na(veg.inv.wide)] <- 0 # empty cells are zero
+veg.inv.wide <- veg.inv.wide[-1] # remove plot id column
 
-Comp.N.Wide <- Comp.N %>%
-  spread(SPECIES_CODE, PERCENT_COVER)
-Comp.N.Wide[is.na(Comp.N.Wide)] <- 0
-Comp.N.Wide = Comp.N.Wide[-1]
+# CALCULATIONS
 
-Ref.Wide <- Ref %>%
-  spread(SPECIES_CODE, PERCENT_COVER)
-Ref.Wide[is.na(Ref.Wide)] <- 0
-Ref.Wide = Ref.Wide[-1]
+# mean height of tallest Carex lyngbyei per plot
+lyngbyHeight <- mean(veg$MAX_LH_CM)
+lyngbyHeight
 
-Ref.N.Wide <- Ref.N %>%
-  spread(SPECIES_CODE, PERCENT_COVER)
-Ref.N.Wide[is.na(Ref.N.Wide)] <- 0
-Ref.N.Wide = Ref.N.Wide[-1]
+# richness (native and total)
+richness <- specnumber(veg.wide)
+richness.nat <- specnumber(veg.nat.wide)
 
-#RICHNESS CALCS
-CompRichness <- specnumber(Comp.Wide)
-CompRichness.N <- specnumber(Comp.N.Wide)
-RefRichness <- specnumber(Ref.Wide)
-RefRichness.N <- specnumber(Ref.N.Wide)
+# shannon-weiner diversity index (native)
+shannon <- diversity(veg.nat.wide, index = "shannon")
 
-#DIVERSITY CALCS
-#SIMPSONS
-CompSimp <- diversity(Comp.Wide, "simpson")
-RefSimp <- diversity(Ref.Wide, "simpson")
-#SHANNONS
-CompShan <- diversity(Comp.Wide, "shannon")
-RefShan <- diversity(Ref.Wide, "shannon")
+# simpson's diversity index (native)
+simpson <- diversity(veg.nat.wide, index = "simpson")
 
-#calculating mean diversity of each site
-mean(CompSimp)
-mean(RefSimp)
+# data summary for reporting relative abundance
+veg.summary <- as.data.frame(rowSums(veg.wide), row.names = "TOTAL")
+veg.summary$NATIVE <- rowSums(veg.nat.wide)
+veg.summary$INV <- rowSums(veg.inv.wide)
 
-mean(CompShan)
-mean(RefShan)
-
-
-#export diversity tables
-write.csv(CompSimp, file ="FILEPATH" )
