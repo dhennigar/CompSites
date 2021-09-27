0378c99c758a1102aa62905c197b2c0c415702bc
Daniel Stewart <danielwork@Daniel-Stewarts-Macbook-Pro.local>
2021-09-27 14:31:10 -0700

    code update

--- Scripts/linearmodel_template.R
+++ Scripts/linearmodel_template.R
@@ -13,93 +13,46 @@ library("LMERConvenienceFunctions")
 library("visreg")
 library("MuMIn")
 
+##NOTE: THIS IS A WORK IN PROGRESS
 
-#NOTE: THIS IS A WORK IN PROGRESS: AS OF 09/22 THIS IS A COMPILATION OF CODE FRAGMENTS!
+#LOADING MASTER DATA .CSV 
+MASTERDATA <- read.csv("~/Documents/R/CompSites/FieldData/SiteData_Master.csv") 
 
-#LOADING .CSV 
-MASTERDATA <- read.csv("~/Documents/R/CompSites/FieldData/SiteData_Example.csv") 
-
+#Creating Compsite Subset (No REF Sites) 
 COMPSITES <- MASTERDATA %>%
   filter(REFERENCE == "NO") 
 
-#%>%
- 
-# group_by(YEAR, TREATMENTCODE)
-#JULYDATA$YEAR = as.factor(JULYDATA$YEAR)
-#JULYDATA$TREATMENT1 = as.factor(JULYDATA$TREATMENT1)
-#JULYDATA = JULYDATA[complete.cases(JULYDATA),]
+#Creating Fraser Subset (No Serp/Nico) 
+FRECOMPSITES <- COMPSITES %>%
+  filter(RIVER != "Serpentine") %>%
+  filter(RIVER != "Nicomekl")
 
-### STEP 2: DATA ANALYSIS (JULY) ### 
+##Research Question #1: What factors affect marshes being vegetated?
+#MODEL 1: Percent Marsh
+MODEL1 <- lm(PRCNT_MARSH ~ (TYPE + LOG_FENCE + SHEAR_BOOM + AGE + AREA_MAPPED + OFFSHORE_STRUCTURE + DIST_UPRIVER)^3, data = FRECOMPSITES)
 
-#JULY: one extreme outlier, but left because they are not likely to be consequential
-outliers <- COMPSITES %>%
-  identify_outliers(ELEV_MEAN)
-view(outliers)
+summary(MODEL1)
+anova(MODEL1)
 
-#checking normality assumption
-#JULY
-ggqqplot(JULYDATA, "ELEV_MEAN", ggtheme = theme_bw()) +
-  facet_grid(YEAR ~ TREATMENTCODE, labeller = "label_both")
-fit.gamma <- fitdist(JULYDATA$RAMETS, distr = "gamma", method = "mme")
-plot(fit.gamma)
-
-
-#SHAPIRO-WILK TEST FOR NORMAL DISTRIBUTION
-JULYDATA %>% 
-  shapiro_test(RAMETS)
-
-#had p values <.05, indicating non-normal distributions 
-#we are dealing with a gamma distribution + count data so GLMER is our model of choice
-
-#initial July model - includes 2-way interactions
-JULYRAMETMODEL <- glmer(1+RAMETS ~ (TREATMENTCODE+YEAR+ELEVATION)^2 + (1|PLOT), data = JULYDATA,
-                        family=poisson(link="log"))
-
-#failed to converge: TROUBLESHOOTING
-#singularity check
-theta <- getME(JULYRAMETMODEL,"theta")
-lower <- getME(JULYRAMETMODEL,"lower")
-min(theta[lower==0])
-#because none of the theta paramaters ==0 or are super close (<.001), we know singularity is not an issue
-
-#experimented with increasing iterations to 25,000 (no avail)
-JULYRAMETMODEL <- glmer(1+RAMETS ~ (TREATMENTCODE+YEAR+ELEVATION)^2 + (1|PLOT), data = JULYDATA,
-                        family=poisson(link="log"),control=glmerControl(optCtrl=list(maxfun=2e5)))
-#experimented with changing optimizer to "bobyqa" (success!)
-JULYRAMETMODEL <- glmer(1+RAMETS ~ (TREATMENTCODE+YEAR+ELEVATION)^2 + (1|PLOT), data = JULYDATA,
-                        family=poisson(link="log"),control=glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e5)))
-#PROCEEDING WITH MODEL
-JULYRAMETMODEL <- glmer(1+RAMETS ~ (TREATMENTCODE+YEAR+ELEVATION)^3 + (1|PLOT), data = JULYDATA,
-                        family=poisson(link="log"),control=glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e5)))
-summary(JULYRAMETMODEL)
-
-
-
-
-
-#data are overdispersed, as value is > 1.0. Proceeding to Quasi-poisson
-#JULYRAMETMODEL <- glmer.nb(1+RAMETS ~ (TREATMENTCODE+YEAR+ELEVATION)^3 + (1|PLOT), data = JULYDATA,
-#                        family=quasipoisson(link="log"),control=glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e5)))
-#summary(JULYRAMETMODEL)
-
-
-#performing dredge for opitmal models
+#performing dredge for optimal models
 options(na.action="na.fail")
-JRM_DREDGE <- dredge(JULYRAMETMODEL, rank = "AICc")
-JRM_DREDGE
+DREDGE <- dredge(MODEL, rank = "AICc")
+DREDGE
 
 #isolating best model
-bestJULYRAMETMODEL <- get.models(JRM_DREDGE,1)[[1]]
-JULYRAMETMODEL_FINAL <- glmer(bestJULYRAMETMODEL, nAGQ = 1, data = JULYDATA,
+bestMODEL <- get.models(DREDGE,1)[[1]]
+summary(bestMODEL)
+
+JULYRAMETMODEL_FINAL <- glmer(bestMODEL, nAGQ = 1, data = JULYDATA,
                               family=poisson(link="log"),control=glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e5)))
 summary(JULYRAMETMODEL_FINAL)
 
 ##DIAGNOSTICS##
 
 #plot residuals - looks good
-coef(JULYRAMETMODEL_FINAL)
-resid(JULYRAMETMODEL_FINAL)
-qqnorm(residuals(JULYRAMETMODEL_FINAL))
+coef(MODEL)
+resid(MODEL)
+qqnorm(residuals(MODEL))
 qqline(residuals(JULYRAMETMODEL_FINAL))
 residualPlots(JULYRAMETMODEL_FINAL)
 shapiro_test(residuals(JULYRAMETMODEL_FINAL))
@@ -191,6 +144,67 @@ tab_df(JULYRAMETMODELANOVA, digits = 10,file="~/Git/TyphaData/FieldData/MODELOUT
 tab_df(JULYRAMETMODELcon,digits = 10, file="~/Git/TyphaData/FieldData/MODELOUTPUTS/JULYRAMETMODELcon.xls")
 tab_df(AUGUSTRAMETMODELANOVA, digits = 10,file="~/Git/TyphaData/FieldData/MODELOUTPUTS/AUGUSTRAMETANOVA.xls" )
 tab_df(AUGUSTRAMETMODELcon,digits = 10, file="~/Git/TyphaData/FieldData/MODELOUTPUTS/AUGUSTRAMETMODELcon.xls")
+
+
+
+OLD CODE FROM MASTERS:
+  
+  #%>%
+  # group_by(YEAR, TREATMENTCODE)
+  #JULYDATA$YEAR = as.factor(JULYDATA$YEAR)
+  #JULYDATA$TREATMENT1 = as.factor(JULYDATA$TREATMENT1)
+  #JULYDATA = JULYDATA[complete.cases(JULYDATA),]
+  
+  ### STEP 2: DATA ANALYSIS (JULY) ### 
+  
+  #JULY: one extreme outlier, but left because they are not likely to be consequential
+  outliers <- COMPSITES %>%
+  identify_outliers(ELEV_MEAN)
+view(outliers)
+
+#checking normality assumption
+#JULY
+ggqqplot(JULYDATA, "ELEV_MEAN", ggtheme = theme_bw()) +
+  facet_grid(YEAR ~ TREATMENTCODE, labeller = "label_both")
+fit.gamma <- fitdist(JULYDATA$RAMETS, distr = "gamma", method = "mme")
+plot(fit.gamma)
+
+
+#SHAPIRO-WILK TEST FOR NORMAL DISTRIBUTION
+JULYDATA %>% 
+  shapiro_test(RAMETS)
+
+#had p values <.05, indicating non-normal distributions 
+#we are dealing with a gamma distribution + count data so GLMER is our model of choice
+
+#initial July model - includes 2-way interactions
+JULYRAMETMODEL <- glmer(RC_NATIVE ~ (TREATMENTCODE+YEAR+ELEVATION)^2 + (1|PLOT), data = JULYDATA,
+                        family=poisson(link="log"))
+
+#failed to converge: TROUBLESHOOTING
+#singularity check
+theta <- getME(JULYRAMETMODEL,"theta")
+lower <- getME(JULYRAMETMODEL,"lower")
+min(theta[lower==0])
+#because none of the theta paramaters ==0 or are super close (<.001), we know singularity is not an issue
+
+#experimented with increasing iterations to 25,000 (no avail)
+JULYRAMETMODEL <- glmer(1+RAMETS ~ (TREATMENTCODE+YEAR+ELEVATION)^2 + (1|PLOT), data = JULYDATA,
+                        family=poisson(link="log"),control=glmerControl(optCtrl=list(maxfun=2e5)))
+#experimented with changing optimizer to "bobyqa" (success!)
+JULYRAMETMODEL <- glmer(1+RAMETS ~ (TREATMENTCODE+YEAR+ELEVATION)^2 + (1|PLOT), data = JULYDATA,
+                        family=poisson(link="log"),control=glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e5)))
+#PROCEEDING WITH MODEL
+JULYRAMETMODEL <- glmer(1+RAMETS ~ (TREATMENTCODE+YEAR+ELEVATION)^3 + (1|PLOT), data = JULYDATA,
+                        family=poisson(link="log"),control=glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e5)))
+summary(JULYRAMETMODEL)
+
+
+###CONTINUED
+
+
+
+
 
 
 
